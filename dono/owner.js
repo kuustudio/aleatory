a805d922475701d17d9de05a2eb72e925dcd965d
aleatory <josivalribeiroalves@gmail.com>
2021-09-05 16:57:46 -0300

    jajaja

---
+++ dono/owner.js
@@ -0,0 +1,16 @@
+const owner = (prefix, ownerNumber, NamaBot) => {
+	return`
+
+╔════ ◤ *INFO DONO/BOT*◢
+║╔▸ 
+║╠ Nome :「 ${NamaOwner} 」
+║╠ Donos : ${ownerNumber} 
+║╠ Youtube :「 ${ytb} 」
+║╠ Instagram : ${instagram} 
+║╠ Prefix : ${prefix} 
+║╚▸
+╚════ ◤ *${NamaBot}* ◢
+`
+}
+
+exports.owner = owner
